6f177fce0a4c53e14eb12043fc8ac436fe247cdd
sysadmin codingcollective <prastowoardi616@gmail.com>
2025-02-18 10:55:08 +0700

    update env and deposit

--- API/Config/config.js
+++ API/Config/config.js
@@ -1,13 +1,17 @@
 import dotenv from 'dotenv';
 
 // Menentukan environment (default: production)
-const envFile = process.env.NODE_ENV === "staging.inr" ? ".env.staging" : ".env.production";
-dotenv.config({ path: envFile });
+const env = process.env.NODE_ENV === "staging.inr" ? ".env.staging" : ".env.production";
+dotenv.config({ path: env });
 
 export const BASE_URL = process.env.BASE_URL;
-export const MERCHANT_CODE = process.env.MERCHANT_CODE;
-export const SECRET_KEY = process.env.SECRET_KEY;
+export const SECRET_KEY_INR = process.env.SECRET_KEY_INR;
+export const DEPOSIT_METHOD_INR = process.env.DEPOSIT_METHOD_INR;
+export const MERCHANT_CODE_INR = process.env.MERCHANT_CODE_INR
+export const MERCHANT_CODE_VND = process.env.MERCHANT_CODE_VND
+export const SECRET_KEY_VND = process.env.SECRET_KEY_VND;
+export const DEPOSIT_METHOD_VND = process.env.DEPOSIT_METHOD_VND;
 
-if (!SECRET_KEY) {
+if (!SECRET_KEY_INR || !SECRET_KEY_VND) {
     throw new Error("SECRET_KEY is required and cannot be empty.");
 }
--- API/deposit.js
+++ API/deposit.js
@@ -2,7 +2,7 @@ import fetch from "node-fetch";
 import readlineSync from "readline-sync";
 import { randomInt } from "crypto";
 import { encryptData } from "../API/utils.js";
-import { BASE_URL, MERCHANT_CODE, SECRET_KEY } from "../API/Config/config.js";
+import { BASE_URL, SECRET_KEY_INR, SECRET_KEY_VND, DEPOSIT_METHOD_INR, DEPOSIT_METHOD_VND, MERCHANT_CODE_INR, MERCHANT_CODE_VND} from "../API/Config/config.js";
 
 async function sendDeposit() {
     console.log("\n=== DEPOSIT REQUEST ===");
@@ -11,33 +11,49 @@ async function sendDeposit() {
 
     const amount = readlineSync.question("Masukkan Amount: ");
     const currency = readlineSync.question("Masukkan Currency (INR/VND): ").toUpperCase();
-    const depositMethod = readlineSync.question("Masukkan Payment Code: ");
+
+    let merchantCode, depositMethod, secretKey;
+    if (currency === "INR") {
+        merchantCode = MERCHANT_CODE_INR;
+        depositMethod = DEPOSIT_METHOD_INR;
+        secretKey = SECRET_KEY_INR;
+    } else if (currency === "VND") {
+        merchantCode = MERCHANT_CODE_VND;
+        depositMethod = DEPOSIT_METHOD_VND;
+        secretKey = SECRET_KEY_VND;
+    } else {
+        console.log("Please check merchant code or deposit method!");
+        return;
+    }
 
     const timestamp = Math.floor(Date.now() / 1000).toString();
-    const transactionCode = `TEST-DEP-${timestamp}`;
+    const transactionCode = `TEST-DP-${timestamp}`;
 
     const payload = {
-        merchant_code: MERCHANT_CODE,
+        merchant_code: merchantCode,
         transaction_code: transactionCode,
         transaction_timestamp: timestamp,
         transaction_amount: amount,
         user_id: userID.toString(),
         currency_code: currency,
-        payout_code: depositMethod
+        deposit_method: depositMethod
     };
 
-    const encryptedPayload = encryptData(payload, SECRET_KEY);
+    const encryptedPayload = encryptData(payload, secretKey);
 
     try {
-        const response = await fetch(`${BASE_URL}/deposit/${MERCHANT_CODE}`, {
+        const response = await fetch(`${BASE_URL}/api/${merchantCode}/v3/dopayment`, { //?key=${encryptedPayload}`, {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ key: encryptedPayload })
         });
 
         if (!response.ok) {
+            console.log(`Response body: ${await response.text()}`);
+            console.log(merchantCode)
+            console.log(secretKey)
             throw new Error(`HTTP error! Status: ${response.status}`);
-        }
+        }        
 
         const result = await response.json();
         console.log("\nDeposit Response:", result);
