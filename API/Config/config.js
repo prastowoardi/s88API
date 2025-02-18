717209459b1b0185dc5462492ca6588073a24f79
sysadmin codingcollective <prastowoardi616@gmail.com>
2025-02-18 13:58:26 +0700

    payout

--- API/Config/config.js
+++ API/Config/config.js
@@ -7,10 +7,12 @@ dotenv.config({ path: env });
 export const BASE_URL = process.env.BASE_URL;
 export const SECRET_KEY_INR = process.env.SECRET_KEY_INR;
 export const DEPOSIT_METHOD_INR = process.env.DEPOSIT_METHOD_INR;
+export const PAYOUT_METHOD_INR = process.env.PAYOUT_METHOD_INR;
 export const MERCHANT_CODE_INR = process.env.MERCHANT_CODE_INR
 export const MERCHANT_CODE_VND = process.env.MERCHANT_CODE_VND
 export const SECRET_KEY_VND = process.env.SECRET_KEY_VND;
 export const DEPOSIT_METHOD_VND = process.env.DEPOSIT_METHOD_VND;
+export const PAYOUT_METHOD_VND = process.env.PAYOUT_METHOD_VND;
 
 if (!SECRET_KEY_INR || !SECRET_KEY_VND) {
     throw new Error("SECRET_KEY is required and cannot be empty.");
---
+++ API/payout.js
@@ -0,0 +1,79 @@
+import fetch from "node-fetch"; 
+import readlineSync from "readline-sync";
+import { randomInt } from "crypto";
+import { encryptData } from "../API/utils.js";
+import { BASE_URL, SECRET_KEY_INR, SECRET_KEY_VND, PAYOUT_METHOD_INR, PAYOUT_METHOD_VND, MERCHANT_CODE_INR, MERCHANT_CODE_VND } from "../API/Config/config.js";
+
+async function sendPayout() {
+    console.log("\n=== PAYOUT REQUEST ===");
+
+    const userID = randomInt(100, 999); 
+
+    const amount = readlineSync.question("Masukkan Amount: ");
+    const currency = readlineSync.question("Masukkan Currency (INR/VND): ").toUpperCase();
+    
+    const timestamp = Math.floor(Date.now() / 1000).toString();
+    const transactionCode = `TEST-WD-${timestamp}`;
+
+    let merchantCode, payoutMethod, payload;
+
+    if (currency === "INR") {
+        merchantCode = MERCHANT_CODE_INR;
+        payoutMethod = PAYOUT_METHOD_INR;
+        payload = {
+            merchant_code: merchantCode,
+            transaction_code: transactionCode,
+            transaction_timestamp: timestamp,
+            transaction_amount: amount,
+            user_id: userID.toString(),
+            currency_code: currency,
+            bank_account_number: "11133322",
+            ifsc_code: "HDFC0CAACOB",
+            account_name: "Johny",
+            payout_code: payoutMethod,
+            secretKey: SECRET_KEY_INR
+        };
+    } else if (currency === "VND") {
+        merchantCode = MERCHANT_CODE_VND;
+        payoutMethod = PAYOUT_METHOD_VND;
+        payload = {
+            merchant_code: merchantCode,
+            transaction_code: transactionCode,
+            transaction_timestamp: timestamp,
+            transaction_amount: amount,
+            user_id: userID.toString(),
+            currency_code: currency,
+            bank_account_number: "2206491508",
+            bank_code: "970418",
+            account_name: "BUI THOA HOA",
+            payout_code: payoutMethod,
+            secretKey: SECRET_KEY_VND 
+        };
+    } else {
+        console.error("Currency not supported!");
+        return;
+    }
+
+    const encryptedPayload = encryptData(payload, currency === "INR" ? SECRET_KEY_INR : SECRET_KEY_VND);
+
+    try {
+        const response = await fetch(`${BASE_URL}/api/v1/payout/${merchantCode}`, {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ key: encryptedPayload })
+        });
+
+        if (!response.ok) {
+            const errorText = await response.text();
+            console.error(`Error response body: ${errorText}`);
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const result = await response.json();
+        console.log("\nPayout Response:", result);
+    } catch (error) {
+        console.error("\nPayout Error:", error);
+    }
+}
+
+sendPayout();
