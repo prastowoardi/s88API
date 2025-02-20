8e09b2bdaaf538219b563f6a3179ad99f995f2d9
Moon <moon@yopmail.com>
2025-02-20 15:54:55 +0700

    update encrypt decrypt method

--- API/utils.js
+++ API/utils.js
@@ -1,40 +1,29 @@
-import crypto from 'crypto';
-import { SECRET_KEY_INR, SECRET_KEY_VND, MERCHANT_CODE_INR, MERCHANT_CODE_VND, MERCHANT_API_KEY_VND, MERCHANT_API_KEY_INR } from "../API/Config/config.js";
+import CryptoJS from 'crypto-js';
 
-export function encryptData(data, secretKey) {
-    if (!secretKey) {
-        throw new Error("SECRET_KEY is required and cannot be empty.");
+export const encryptDecrypt = (action, data, apikey, secretkey) => {
+    const key = CryptoJS.SHA256(apikey); 
+    const iv = CryptoJS.enc.Utf8.parse(CryptoJS.SHA256(secretkey).toString(CryptoJS.enc.Hex).substring(0, 16)); 
+
+    if (!['encrypt', 'decrypt'].includes(action)) {
+        console.error("Invalid action. Use 'encrypt' or 'decrypt'.");
+        return null;
     }
 
-    const keyBuffer = crypto.createHash('sha256').update(secretKey).digest();
-
-    const iv = crypto.createHash('sha256').update(secretKey, 'utf8').digest().slice(0, 16);
-
-    const cipher = crypto.createCipheriv("aes-256-cbc", keyBuffer, iv);
-
-    let encrypted = cipher.update(data, "utf8", "base64");
-    encrypted += cipher.final("base64");
-
-    encrypted = encrypted.replace(/\+/g, '%2B').replace(/\//g, '%2F').replace(/=/g, '%3D');
-
-    return encrypted;
-}
-
-export function decryptData(encryptedData, secretKey) {
-    if (!secretKey) {
-        throw new Error("SECRET_KEY is required and cannot be empty.");
+    if (action === "encrypt") {
+        return encodeURIComponent(
+            CryptoJS.AES.encrypt(data, key, {
+                iv,
+                mode: CryptoJS.mode.CBC,
+                padding: CryptoJS.pad.Pkcs7
+            }).toString()
+        );
     }
 
-    const keyBuffer = crypto.createHash('sha256').update(secretKey).digest();
-
-    const iv = crypto.createHash('sha256').update(secretKey, 'utf8').digest().slice(0, 16);
-
-    const decipher = crypto.createDecipheriv("aes-256-cbc", keyBuffer, iv);
-
-    const decodedData = encryptedData.replace(/%2B/g, '+').replace(/%2F/g, '/').replace(/%3D/g, '=');
-
-    let decrypted = decipher.update(decodedData, "base64", "utf8");
-    decrypted += decipher.final("utf8");
-
-    return decrypted;
-}
+    if (action === "decrypt") {
+        return CryptoJS.AES.decrypt(decodeURIComponent(data), key, {
+            iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }).toString(CryptoJS.enc.Utf8);
+    }
+};
