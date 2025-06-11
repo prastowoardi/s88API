14ec1e087aa73ad033a4e7aecd7713b32f992ed7
Moon <moon@yopmail.com>
2025-06-11 21:18:41 +0700

    logger

---
+++ API/logger.js
@@ -0,0 +1,41 @@
+import winston from "winston";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.combine(
+    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console({
+      level: "info",
+      format: winston.format.combine(
+        winston.format.colorize(),
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.printf(({ timestamp, level, message }) => {
+          
+          return `[${timestamp}] ${level}: ${message}`;
+        })
+      )
+    }),
+    new winston.transports.File({
+      filename: path.join(__dirname, "log", `${new Date().toISOString().slice(0,10)}.log`),
+      level: "info",
+      format: winston.format.combine(
+        winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+        winston.format.printf(({ timestamp, level, message }) => {
+          return `[${timestamp}] ${level.toUpperCase()}: ${message}`;
+        })
+      )
+    }),
+  ],
+});
+
+export default logger;
--- API/trx-payout/manualINR.js
+++ API/trx-payout/manualINR.js
@@ -1,36 +1,46 @@
 import fetch from "node-fetch";
 import readlineSync from "readline-sync";
+import logger from "../logger.js";
 import { randomInt } from "crypto";
 import { encryptDecryptPayout } from "../helpers/utils.js";
 import { BASE_URL, SECRET_KEY_INR, PAYOUT_METHOD_INR, MERCHANT_CODE_INR, MERCHANT_API_KEY_INR } from "../Config/config.js";
-import { getValidIFSC, getRandomName, } from "../helpers/payoutHelper.js";
+import { getValidIFSC, getRandomName } from "../helpers/payoutHelper.js";
 
 const ifscCode = await getValidIFSC();
 console.log(`IFSC Code: ${ifscCode}`);
 
 async function sendPayout() {
-    console.log("\n=== PAYOUT REQUEST ===");
+  logger.info("======== INR - MANUAL PAYOUT REQUEST ========");
 
-    const amount = readlineSync.question("Masukkan Amount: ");
-    if (isNaN(amount) || Number(amount) <= 0) {
-        console.error("❌ Amount tidak valid.");
-        return;
-    }
+  const amount = readlineSync.question("Masukkan Amount: ");
+  logger.info(`Amount input: ${amount}`);
+  if (isNaN(amount) || Number(amount) <= 0) {
+    console.error("❌ Amount tidak valid.");
+    return;
+  }
 
     // Input semua data
     // const userID = readlineSync.question("Masukkan User ID: ");
+    // logger.info(`UserID Input: ${userID}`);
+    
     // const bankAccountNumber = readlineSync.question("Masukkan Nomor Rekening: ");
+    // logger.info(`Bank Account Number input: ${bankAccountNumber}`);
+    
     // const accountName = readlineSync.question("Masukkan Nama Akun: ");
+    // logger.info(`Account Name input: ${accountName}`);
+    
     // const ifscCode = readlineSync.question("Masukkan IFSC Code: ");
+    // logger.info(`IFSC Code input: ${ifscCode}`);
+    
+    // const transactionCode = readlineSync.question("Masukkan Transaction Code: ")
+    // logger.info(`Transaction Code input: ${transactionCode}`);
+
     const timestamp = Math.floor(Date.now() / 1000).toString();
     const transactionCode = `TEST-WD-${timestamp}`;
-    
-    // Input transaction code
+
     const accountName = await getRandomName();
     const bankAccountNumber = 111111;
     const userID = randomInt(100);
-    // const transactionCode = readlineSync.question("Masukkan Transaction Code: ")
-
 
     const merchantCode = MERCHANT_CODE_INR;
     const payoutMethod = PAYOUT_METHOD_INR;
@@ -50,25 +60,40 @@ async function sendPayout() {
         payout_code: payoutMethod,
     };
 
-    console.log(`\n🔗 URL: ${BASE_URL}/api/v1/payout/${merchantCode}`);
-    console.log("\n📜 Request Payload:", JSON.stringify(payload, null, 2));
+    logger.info(`🔗 URL: ${BASE_URL}/api/v1/payout/${merchantCode}`);
+    logger.info(`📜 Request Payload:\n${JSON.stringify(payload, null, 2)}`);
 
     const encryptedPayload = encryptDecryptPayout("encrypt", payload, apiKey, secretKey);
-    console.log(`\n🔑 Encrypted Key:`, encryptedPayload);
+    logger.info(`🔑 Encrypted Key: ${encryptedPayload}`);
 
     try {
         const response = await fetch(`${BASE_URL}/api/v1/payout/${merchantCode}`, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ key: encryptedPayload }),
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ key: encryptedPayload }),
         });
 
-        const result = await response.json();
-        console.log("\n📥 Payout Response:", result);
-        console.log("\n⚡️Response Status: ", response.status);
+        let resultText;
+        try {
+        resultText = await response.text();
+        const result = JSON.parse(resultText);
+
+        if (!response.ok) {
+            logger.warn(`⚠️ HTTP Error ${response.status}`);
+        }
+
+        logger.info(`📥Payout Response\n${JSON.stringify(result, null, 2)}`);
+        logger.info(`⚡️Response Status: ${response.status}`);
+        } catch (parseErr) {
+        logger.error("❌ Gagal parsing JSON response");
+        logger.error("Raw response:\n" + resultText);
+        logger.error("Error detail: " + parseErr.message);
+        }
     } catch (error) {
-        console.error("\n❌ Payout Error:", error.message);
+        logger.error("❌ Payout Error: " + error.message);
     }
+
+    logger.info("======== REQUEST DONE ========");
 }
 
 sendPayout();
