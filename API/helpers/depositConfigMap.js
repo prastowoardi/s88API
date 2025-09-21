7256fe9c6728841e1c566b2e2c45ea180551381c
Moon <moon@yopmail.com>
2025-09-21 17:16:31 +0700

    simplyfy

--- API/helpers/depositConfigMap.js
+++ API/helpers/depositConfigMap.js
@@ -1,4 +1,4 @@
-import {
+import { 
   BASE_URL,
   CALLBACK_URL,
   MERCHANT_CODE_INR, SECRET_KEY_INR, MERCHANT_API_KEY_INR, DEPOSIT_METHOD_INR,
@@ -19,88 +19,24 @@ const defaultConfig = {
   callbackURL: CALLBACK_URL,
 };
 
-export const currencyConfigMap = {
-  INR: {
-    merchantCode: MERCHANT_CODE_INR,
-    depositMethod: DEPOSIT_METHOD_INR,
-    secretKey: SECRET_KEY_INR,
-    merchantAPI: MERCHANT_API_KEY_INR,
-  },
-  VND: {
-    merchantCode: MERCHANT_CODE_VND,
-    depositMethod: DEPOSIT_METHOD_VND,
-    secretKey: SECRET_KEY_VND,
-    merchantAPI: MERCHANT_API_KEY_VND,
-    requiresBankCode: true,
-  },
-  BDT: {
-    merchantCode: MERCHANT_CODE_BDT,
-    depositMethod: DEPOSIT_METHOD_BDT,
-    secretKey: SECRET_KEY_BDT,
-    merchantAPI: MERCHANT_API_KEY_BDT,
-    bankCodeOptions: ["1002", "1001", "1004", "1003"],
-  },
-  MMK: {
-    merchantCode: MERCHANT_CODE_MMK,
-    depositMethod: DEPOSIT_METHOD_MMK,
-    secretKey: SECRET_KEY_MMK,
-    merchantAPI: MERCHANT_API_KEY_MMK,
-    requiresBankCode: true,
-  },
-  BRL: {
-    merchantCode: MERCHANT_CODE_BRL,
-    depositMethod: DEPOSIT_METHOD_BRL,
-    secretKey: SECRET_KEY_BRL,
-    merchantAPI: MERCHANT_API_KEY_BRL,
-    requiresBankCode: true,
-  },
-  IDR: {
-    merchantCode: MERCHANT_CODE_IDR,
-    depositMethod: DEPOSIT_METHOD_IDR,
-    secretKey: SECRET_KEY_IDR,
-    merchantAPI: MERCHANT_API_KEY_IDR,
-    requiresBankCode: true,
-  },
-  THB: {
-    merchantCode: MERCHANT_CODE_THB,
-    depositMethod: DEPOSIT_METHOD_THB,
-    secretKey: SECRET_KEY_THB,
-    merchantAPI: MERCHANT_API_KEY_THB,
-    requiresBankCode: true,
-    cardNumber: true,
-  },
-  MXN: {
-    merchantCode: MERCHANT_CODE_MXN,
-    depositMethod: DEPOSIT_METHOD_MXN,
-    secretKey: SECRET_KEY_MXN,
-    merchantAPI: MERCHANT_API_KEY_MXN,
-    requiresBankCode: true,
-  },
-  KRW: {
-    merchantCode: MERCHANT_CODE_KRW,
-    depositMethod: DEPOSIT_METHOD_KRW,
-    secretKey: SECRET_KEY_KRW,
-    merchantAPI: MERCHANT_API_KEY_KRW,
-    requiresBankCode: false,
-  },
-  PHP: {
-    merchantCode: MERCHANT_CODE_PHP,
-    depositMethod: DEPOSIT_METHOD_PHP,
-    secretKey: SECRET_KEY_PHP,
-    merchantAPI: MERCHANT_API_KEY_PHP,
-    requiresBankCode: true,
-  },
-  HKD: {
-    merchantCode: MERCHANT_CODE_HKD,
-    depositMethod: DEPOSIT_METHOD_HKD,
-    secretKey: SECRET_KEY_HKD,
-    merchantAPI: MERCHANT_API_KEY_HKD,
-  }
+const currencyConfigs = {
+  INR: { merchantCode: MERCHANT_CODE_INR, depositMethod: DEPOSIT_METHOD_INR, secretKey: SECRET_KEY_INR, merchantAPI: MERCHANT_API_KEY_INR },
+  VND: { merchantCode: MERCHANT_CODE_VND, depositMethod: DEPOSIT_METHOD_VND, secretKey: SECRET_KEY_VND, merchantAPI: MERCHANT_API_KEY_VND, requiresBankCode: true },
+  BDT: { merchantCode: MERCHANT_CODE_BDT, depositMethod: DEPOSIT_METHOD_BDT, secretKey: SECRET_KEY_BDT, merchantAPI: MERCHANT_API_KEY_BDT, requiresBankCode: true },
+  MMK: { merchantCode: MERCHANT_CODE_MMK, depositMethod: DEPOSIT_METHOD_MMK, secretKey: SECRET_KEY_MMK, merchantAPI: MERCHANT_API_KEY_MMK, requiresBankCode: true },
+  BRL: { merchantCode: MERCHANT_CODE_BRL, depositMethod: DEPOSIT_METHOD_BRL, secretKey: SECRET_KEY_BRL, merchantAPI: MERCHANT_API_KEY_BRL, requiresBankCode: true },
+  IDR: { merchantCode: MERCHANT_CODE_IDR, depositMethod: DEPOSIT_METHOD_IDR, secretKey: SECRET_KEY_IDR, merchantAPI: MERCHANT_API_KEY_IDR, requiresBankCode: true },
+  THB: { merchantCode: MERCHANT_CODE_THB, depositMethod: DEPOSIT_METHOD_THB, secretKey: SECRET_KEY_THB, merchantAPI: MERCHANT_API_KEY_THB, requiresBankCode: true, cardNumber: true },
+  MXN: { merchantCode: MERCHANT_CODE_MXN, depositMethod: DEPOSIT_METHOD_MXN, secretKey: SECRET_KEY_MXN, merchantAPI: MERCHANT_API_KEY_MXN, requiresBankCode: true },
+  KRW: { merchantCode: MERCHANT_CODE_KRW, depositMethod: DEPOSIT_METHOD_KRW, secretKey: SECRET_KEY_KRW, merchantAPI: MERCHANT_API_KEY_KRW, requiresBankCode: false },
+  PHP: { merchantCode: MERCHANT_CODE_PHP, depositMethod: DEPOSIT_METHOD_PHP, secretKey: SECRET_KEY_PHP, merchantAPI: MERCHANT_API_KEY_PHP, requiresBankCode: true },
+  HKD: { merchantCode: MERCHANT_CODE_HKD, depositMethod: DEPOSIT_METHOD_HKD, secretKey: SECRET_KEY_HKD, merchantAPI: MERCHANT_API_KEY_HKD }
 };
 
-for (const currency in currencyConfigMap) {
-  currencyConfigMap[currency] = { ...defaultConfig, ...currencyConfigMap[currency] };
-}
+const currencyConfigMap = Object.keys(currencyConfigs).reduce((map, currency) => {
+  map[currency] = { ...defaultConfig, ...currencyConfigs[currency] };
+  return map;
+}, {});
 
 export function getCurrencyConfig(currency) {
   const config = currencyConfigMap[currency];
@@ -108,6 +44,5 @@ export function getCurrencyConfig(currency) {
     console.error(`Config untuk currency '${currency}' tidak ditemukan.`);
     throw new Error(`❌ Config untuk currency '${currency}' tidak ditemukan.`);
   }
-  // console.log(`Currency Config for ${currency}:`, config);
   return config;
 }
